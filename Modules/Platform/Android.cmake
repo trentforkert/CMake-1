d4a2a23b9d500434e294ad2b8515731aeaee7c30
Ben Boeckel <mathstuf@gmail.com>
2014-04-15 00:29:40 -0400

    Android: Support sonames on Android
    This is actually required because without it, the APK will be bundle up
    any library linking to a library without a SONAME with an absolute path.

--- Modules/Platform/Android.cmake
+++ Modules/Platform/Android.cmake
@@ -1,5 +1,5 @@
 include(Platform/Linux)
-set(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "")
+set(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "-Wl,-soname,")
 # RPath is useless on Android, because we can't determine the installation
 # location ahead of time.
 set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "")
